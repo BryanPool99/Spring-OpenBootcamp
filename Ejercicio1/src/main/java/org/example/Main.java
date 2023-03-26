a012e660e76a94b29516d756316b0820869cb9f6
BryanPool99 <bryanpool991@hotmail.com>
2023-03-25 22:18:54 -0500

    Metodo main

--- Ejercicio1/src/main/java/org/example/Main.java
+++ Ejercicio1/src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/*
+    Crear proyecto maven con la dependencia spring-context y crear una clase Saludo
+    con un método imprimirSaludo que imprima un texto por consola.
+
+    Crear el archivo beans.xml con un bean para la clase Saludo.
+
+    Obtener el bean desde el método main y ejecutar el método imprimirSaludo.
+*/
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Saludo saludar=(Saludo) context.getBean("saludo");
+        saludar.imprimirSaludo();
     }
 }
