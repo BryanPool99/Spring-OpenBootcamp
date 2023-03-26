9fef9e31404c53259884e325c16351567845235f
BryanPool99 <bryanpool991@hotmail.com>
2023-03-25 22:18:38 -0500

    clase saludo

---
+++ Ejercicio1/src/main/java/org/example/Saludo.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Saludo {
+    public Saludo(){
+
+    }
+    public void imprimirSaludo(){
+        System.out.println("Hola a todos desde la clase Saludo.");
+    }
+}
